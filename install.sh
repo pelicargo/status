9e622d0532a15fcdea6bee2fa4239b6c177f319a
RobCubed <officialgithub@4bit.pw>
2025-11-13 17:10:27 -0500

    fix: crash course in systemd

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.config/systemd/user
 cat peli-status.service | sed "s#%PWD#$(pwd)#g" > ~/.config/systemd/user/peli-status.service
+loginctl enable-linger
 systemctl --user enable peli-status
 systemctl --user start peli-status
